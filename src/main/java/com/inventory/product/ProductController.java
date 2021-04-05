0c5e57fbabc3e7b5468d1bdec8b66524b22c02e8
Carlavn84 <dduong.vt@gmail.com>
2021-04-05 17:29:01 +0200

    Undo deleted stuffs

--- src/main/java/com/inventory/product/ProductController.java
+++ src/main/java/com/inventory/product/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="api/v1/product")
+@RequestMapping(path="api/product")
 public class ProductController {
     private final ProductService productService;
 
